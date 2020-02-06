96b9dc3e6e8ef2c63c127402f1d2ee0f3f9c46ac
Jean Tubay <jean.tubay.92@gmail.com>
2020-02-05 21:20:05 -0500

    Updates

---
+++ precache-manifest.b4c61517e042a20d2ceaca3499212c44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23c731981b3a70bef49337ad7935375a",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "78a58d8f5174d1e2def3",
+    "url": "/deploy-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "149574af6e00d241bbc4",
+    "url": "/deploy-react/static/js/2.ae69f4c5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/deploy-react/static/js/2.ae69f4c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78a58d8f5174d1e2def3",
+    "url": "/deploy-react/static/js/main.61209a03.chunk.js"
+  },
+  {
+    "revision": "02546a2d22f11db2cd27",
+    "url": "/deploy-react/static/js/runtime-main.5c40388e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
